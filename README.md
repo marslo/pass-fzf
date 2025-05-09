e42bb14921eca7f456b88f9a3a082644235eb1b3
marslo <marslo.jiao@gmail.com>
2025-05-08 18:05:43 -0700

    refactor!: enhance the plugin and installation instructions
    - refactor fzf.bash to use env bash and support more pass subcommands: ` -c/--clip`,`show`,`edit`,`rm`
    - improve argument parsing and error handling in fzf.bash
    - enable `.releaserc.json` for semantic release
    
    BREAKING CHANGE:  fzf.bash refactoring
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- README.md
+++ README.md
@@ -1,23 +1,53 @@
 # pass-fzf
 An extension for [pass](https://www.passwordstore.org/) that allows fuzzy
-finding in the store.
+finding in the store ( `$PASSWORD_STORE_DIR` ).
 
-requires `fzf` to be installed in the system.
+> [!TIP]
+> requires `fzf` to be installed in the system.
 
 # Installation
-Installation is done through `pass` extensions.
-```
-mkdir -p $PASSWORD_STORE_DIR/.extensions/
-wget https://raw.githubusercontent.com/ficoos/pass-fzf/master/fzf.bash -O $PASSWORD_STORE_DIR/.extensions/fzf.bash
+
+```bash
+curl -fsSL --create-dirs \
+     --create-file-mode 755 \
+     --output $PASSWORD_STORE_DIR/.extensions/fzf.bash \
+     https://github.com/marslo/pass-fzf/raw/master/fzf.bash &&
 chmod +x $PASSWORD_STORE_DIR/.extensions/fzf.bash
-PASSWORD_STORE_ENABLE_EXTENSIONS=true pass fzf
+```
+
+```bash
+# temporarily
+$ PASSWORD_STORE_ENABLE_EXTENSIONS=true pass fzf
+
+# permanently
+$ echo 'export PASSWORD_STORE_ENABLE_EXTENSIONS=true' >> ~/.bashrc
+$ pass fzf
 ```
 
 # Usage
+
+```bash
+$ pass fzf --help
+USAGE: pass fzf [options] [query]
+
+OPTIONS:
+  -s           Only output selected path (don't run 'pass')
+  -h, --help   Show this help message
+
+EXAMPLES:
+  pass fzf                  → pass show <selected>
+  pass fzf show             → pass show <selected>
+  pass fzf github           → fzf search with 'github', then pass show <selected>
+  pass fzf -c               → pass -c <selected>
+  pass fzf edit             → pass edit <selected>
+  pass fzf -s               → just print path
+  pass fzf -s github        → just print path (query='github')
+
+NOTE:
+  • supported pass sub-commands : -c/--clip,show,edit,rm
 ```
-$ pass fzf
-```
+
 # Alias
-```
+```bash
 $ alias passs='PASSWORD_STORE_ENABLE_EXTENSIONS=true pass fzf'
 ```
--- fzf.bash
+++ fzf.bash
@@ -1,41 +1,80 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+declare -a pass_subcommands=(show edit rm)
 
 function candidates() {
-    find "$PREFIX" -name '*.gpg' | sed -e "s:$PREFIX/::gi" -e 's:.gpg$::gi'
+  PREFIX="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
+  find "${PREFIX}" -name '*.gpg' | sed -e "s:$PREFIX/::gi" -e 's:.gpg$::gi'
 }
 
 function candidate_selector_fzf() {
-    query=$1
-    candidates | fzf -q "$query" --select-1
+  query=$1
+  candidates | fzf -q "${query}"
 }
 
 function usage() {
-    echo "Usage: $0 [-s] [query]"
-    exit 1
+    cat <<EOF
+USAGE: pass fzf [options] [query]
+
+OPTIONS:
+  -s           Only output selected path (don't run 'pass')
+  -h, --help   Show this help message
+
+EXAMPLES:
+  pass fzf                  → pass show <selected>
+  pass fzf show             → pass show <selected>
+  pass fzf github           → fzf search with 'github', then pass show <selected>
+  pass fzf -c               → pass -c <selected>
+  pass fzf edit             → pass edit <selected>
+  pass fzf -s               → just print path
+  pass fzf -s github        → just print path (query='github')
+
+NOTE:
+EOF
+printf "  • supported pass sub-commands : -c/--clip,"
+IFS=', '
+echo "${pass_subcommands[*]}"
+  exit 0
 }
 
-select_only=0
+declare -i select_only=0
+declare -a pass_args=()
+declare query=''
 
-while getopts "s" o
-do
-    case "${o}" in
-        s)
-            select_only=1
-            ;;
-        *)
-            usage
-            ;;
-    esac
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -s          ) select_only=1     ; shift ;;
+    -h | --help ) usage                     ;;
+    --          ) shift             ; break ;;
+    -*          ) pass_args+=("$1") ; shift ;;
+    *           ) if [[ -z "${query}" && ! " ${pass_subcommands[*]} " =~ ${1} ]]; then
+                    query="$1"
+                  else
+                    pass_args+=("$1")
+                  fi
+                  shift
+                  ;;
+  esac
 done
 
-shift $((OPTIND-1))
-query="$@"
-
-res=$(candidate_selector_fzf "$query")
-if [ -n "$res" ]; then
-    [ $select_only -ne 0 ] && echo "$res" && exit 0
-    pass show "$res" | tail -n +2 || exit $?
-    pass show -c "$res"
-else
-    exit 1
+if [[ "${#pass_args[@]}" -gt 0 ]]; then
+  first_cmd="${pass_args[0]}"
+  if [[ "${first_cmd}" != -* && ! " ${pass_subcommands[*]} " =~ ${first_cmd} ]]; then
+    echo "Unsupported sub-command '${first_cmd}' with 'pass fzf'" >&2
+    exit 2
+  fi
 fi
+
+res=$(candidate_selector_fzf "${query}")
+
+if [[ -n "${res}" ]]; then
+  if [[ "${select_only}" -ne 0 ]]; then
+    echo "${res}"
+    exit 0
+  fi
+  pass "${pass_args[@]}" "${res}"
+else
+  exit 1
+fi
+
+# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh:
